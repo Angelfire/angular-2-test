14ab5d4ec3f83fd066c3c9143af41c333cf79f1f
Sergio A. Bedoya G <andres.paranoid@gmail.com>
2017-03-30 10:59:07 -0500

    Small change

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -7,8 +7,23 @@ import { Pokemon } from './pokemon';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+   /**
+   * The component maintain a list of Pokemon objects
+   * that will be rendered.
+   *
+   * We initialize it to an empty list.
+   */
   pokemon:Pokemon[] = [];
+
+  /**
+   * A boolean that represents if we are currently loading data.
+   */
   isLoading:boolean = false;
+
+   /**
+   * This boolean will be set to true if an error occurred.
+   */
   error:boolean = false;
 
   constructor(private pokeapiService:PokeapiService) {}
@@ -17,12 +32,16 @@ export class AppComponent {
     this.loadPokemon();
   }
 
+  /**
+   * Use the PokeApiService to load the first pokemons.
+   */
   loadPokemon() {
     this.isLoading = true;
 
     this.pokeapiService.getPokemon(this.pokemon.length, 20)
       .then(pokemon => {
         pokemon = pokemon.map(p => {
+          p.imageLoaded = false;
           return p;
         });
 
